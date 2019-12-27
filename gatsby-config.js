3b72beb278ceed30a3d49d01ec939de0043912ab
ramisas <jorahuev@gmail.com>
2019-12-28 00:19:12 +0300

    update 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,44 +24,45 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        "icons": [
+        icon: `src/images/icon-512x512.png`,
+        icons: [
           {
-            "src": "icons/icon-72x72.png",
+            "src": "/favicons/icon-72x72.png",
             "sizes": "72x72",
             "type": "image/png"
           },
           {
-            "src": "icons/icon-96x96.png",
+            "src": "/favicons/icon-96x96.png",
             "sizes": "96x96",
             "type": "image/png"
           },
           {
-            "src": "icons/icon-128x128.png",
+            "src": "/favicons/icon-128x128.png",
             "sizes": "128x128",
             "type": "image/png"
           },
           {
-            "src": "icons/icon-144x144.png",
+            "src": "/favicons/icon-144x144.png",
             "sizes": "144x144",
             "type": "image/png"
           },
           {
-            "src": "icons/icon-152x152.png",
+            "src": "/favicons/icon-152x152.png",
             "sizes": "152x152",
             "type": "image/png"
           },
           {
-            "src": "icons/icon-192x192.png",
+            "src": "/favicons/icon-192x192.png",
             "sizes": "192x192",
             "type": "image/png"
           },
           {
-            "src": "icons/icon-384x384.png",
+            "src": "/favicons/icon-384x384.png",
             "sizes": "384x384",
             "type": "image/png"
           },
           {
-            "src": "icons/icon-512x512.png",
+            "src": "/favicons/icon-512x512.png",
             "sizes": "512x512",
             "type": "image/png"
           }
